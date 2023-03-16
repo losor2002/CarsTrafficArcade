a1a6df0d09e0a353b47b5a1d2a6993ca0073a721
losor2002 <114603230+losor2002@users.noreply.github.com>
2023-03-16 18:41:02 +0100

    TouchedBonus.cs fixed

---
+++ Assets/_script/TouchedBonus.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace _script
+{
+    public class TouchedBonus : MonoBehaviour
+    {
+        private GameController _gameController;
+        private bool _touched;
+
+        private void Start()
+        {
+            _gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player") && !_touched)
+            {
+                _gameController.Touched();
+                _touched = true;
+            }
+        }
+    }
+}
