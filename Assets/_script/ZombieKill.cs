95308267dffcbf0db5dc5fc18e2592ec11dad878
losor2002 <114603230+losor2002@users.noreply.github.com>
2023-03-16 18:55:41 +0100

    ZombieKill.cs fixed

---
+++ Assets/_script/ZombieKill.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace _script
+{
+    public class ZombieKill : MonoBehaviour
+    {
+        [FormerlySerializedAs("zombiecade")] public GameObject fallingZombie;
+
+        private bool _hit;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("proiettile"))
+            {
+                if (_hit)
+                {
+                    Instantiate(fallingZombie, transform.position, transform.rotation);
+                    PlayerPrefs.SetInt("kills", PlayerPrefs.GetInt("kills", 0) + 1);
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    _hit = true;
+                }
+
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
