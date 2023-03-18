eb788b6b58d1fc5124824a89f6716bfa00a2bdf7
losor2002 <114603230+losor2002@users.noreply.github.com>
2023-03-18 14:14:00 +0100

    VerticalMovementArrow.cs fixed

---
+++ Assets/_script/VerticalMovementArrow.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace _script
+{
+    public class VerticalMovementArrow : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        private GameController _gameController;
+        private bool _isUpArrow;
+        private int _pointerID;
+        private bool _touched;
+
+        private void Start()
+        {
+            _gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+            _isUpArrow = CompareTag("su");
+        }
+
+        public void OnPointerDown(PointerEventData data)
+        {
+            if (_touched)
+            {
+                return;
+            }
+
+            _touched = true;
+            _pointerID = data.pointerId;
+            if (_isUpArrow)
+            {
+                _gameController.verticalPlayerMovement = 1;
+            }
+            else
+            {
+                _gameController.verticalPlayerMovement = -1;
+            }
+        }
+
+        public void OnPointerUp(PointerEventData data)
+        {
+            if (data.pointerId == _pointerID)
+            {
+                _gameController.verticalPlayerMovement = 0;
+                _touched = false;
+            }
+        }
+    }
+}
