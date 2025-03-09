6b3c7a022bd81427ef4f4e38293f005b2536d96b
losor2002 <losor20002@gmail.com>
2025-03-09 01:56:47 +0100

    Refactor TrackSpawn.cs behaviour, Fixed boundary position and scale

--- Assets/CtaScript/SceneManagement/TrackSpawn.cs
+++ Assets/CtaScript/SceneManagement/TrackSpawn.cs
@@ -1,40 +1,22 @@
-﻿using System.Collections;
-using CtaScript.GameController;
-using UnityEngine;
-using UnityEngine.Serialization;
+﻿using UnityEngine;
 
 namespace CtaScript.SceneManagement
 {
     public class TrackSpawn : MonoBehaviour
     {
-        [FormerlySerializedAs("pista")] public GameObject track;
-        [FormerlySerializedAs("spawn")] public Vector3 spawnPosition;
+        public Vector3 spawnPosition;
         public Quaternion spawnRotation;
-        [FormerlySerializedAs("spawntime")] public float spawnInterval;
-        [FormerlySerializedAs("spawntime1")] public float spawnInterval1;
-        [FormerlySerializedAs("spawntime2")] public float spawnInterval2;
+        public float zSpawnThreshold;
+        
+        private bool _spawned;
 
-        private GameControllerPlayScene _gameControllerPlayScene;
-
-        private void Start()
+        private void Update()
         {
-            _gameControllerPlayScene = GetComponent<GameControllerPlayScene>();
-            StartCoroutine(Spawn());
-        }
-
-        private IEnumerator Spawn()
-        {
-            while (!_gameControllerPlayScene.gameOver)
+            if (!_spawned && transform.position.z <= zSpawnThreshold)
             {
-                Instantiate(track, spawnPosition, spawnRotation);
-
-                spawnInterval = _gameControllerPlayScene.score switch
-                {
-                    >= 30 and < 50 => spawnInterval1,
-                    >= 50 => spawnInterval2,
-                    _ => spawnInterval
-                };
-                yield return new WaitForSeconds(spawnInterval);
+                GameObject newTrack = Instantiate(gameObject, spawnPosition, spawnRotation);
+                newTrack.name = gameObject.name;
+                _spawned = true;
             }
         }
     }
