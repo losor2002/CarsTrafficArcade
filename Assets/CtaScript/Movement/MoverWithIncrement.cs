960243c42f6b165adf064f73003d7b828dc897a3
losor2002 <losor20002@gmail.com>
2025-03-09 17:35:04 +0100

    Replaced MoverMultipleSpeeds.cs with MoverWithIncrement.cs, changed zombie speed

---
+++ Assets/CtaScript/Movement/MoverWithIncrement.cs
@@ -0,0 +1,56 @@
+﻿using CtaScript.GameController;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace CtaScript.Movement
+{
+    public class MoverWithIncrement : MonoBehaviour
+    {
+        private const float TrackZombieModeSpeed = -11f;
+        private const float Increment = -1f;
+        
+        public float speed;
+
+        private GameControllerPlayScene _gameControllerPlayScene;
+        private float _trackDeltaSpeed;
+
+        private void Awake()
+        {
+            if (CompareTag("Enemy"))
+            {
+                speed += Random.Range(-1f, 0f);
+            }
+            else if (PlayerPrefs.GetInt("zombie") != 0 && CompareTag("pista"))
+            {
+                speed = TrackZombieModeSpeed;
+            }
+        }
+
+        private void Start()
+        {
+            _gameControllerPlayScene = FindAnyObjectByType<GameControllerPlayScene>();
+            
+            float trackSpeed = GameObject.FindWithTag("pista").GetComponent<MoverWithIncrement>().speed;
+            _trackDeltaSpeed = speed - trackSpeed;
+        }
+
+        private void Update()
+        {
+            if (!_gameControllerPlayScene.gameOver)
+            {
+                float currentSpeed = _gameControllerPlayScene.score switch
+                {
+                    >= 30 and < 50 => speed + Increment,
+                    >= 50 => speed + Increment * 2,
+                    _ => speed
+                };
+                
+                transform.position += Vector3.forward * (currentSpeed * Time.deltaTime);
+            }
+            else
+            {
+                transform.position += Vector3.forward * (_trackDeltaSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
